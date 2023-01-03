4aee450eac0f152d01bcb7b698006f40990b9748
Bohdan <bogdantelep@gmail.com>
2023-01-03 21:22:13 +0300

    adding chatboxes

--- src/components/messages-view/messages-view.js
+++ src/components/messages-view/messages-view.js
@@ -1,9 +1,16 @@
 import './messages-view.css';
+import ChatMessageBox from '../chat-message-box/chat-message-box';
 
 const MessagesView = (props) => {
     return (
         <>
+            <div className="container-messages">
+                <ChatMessageBox type="left"/>
+                <ChatMessageBox type="right"/>
 
+                <ChatMessageBox type="left"/>
+                <ChatMessageBox type="right"/>
+            </div>
         </>
     );
 }
