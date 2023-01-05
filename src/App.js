a979a3a609fc3f12a0b916cabf5ab2581b009cd4
RabotiahovDmytro <you@example.com>
2023-01-05 20:33:54 +0200

    fixed problems caused by merging

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
-import { Login, Register, NotFound404, MainPage, Chats, Profile, AccountRestore } from './components/pages/pages';
-=======
-import { Login, Register, NotFound404, MainPage, Chats, Profile, ProfileEdit, Battles, FAQ } from './components/pages/pages';
->>>>>>> 2ac0169e6b39cfc6c319024a7e127f03a2b55e63
+import { Login, Register, NotFound404, MainPage, Chats, Profile, ProfileEdit, Battles, FAQ, AccountRestore} from './components/pages/pages';
+
 
 function App() {
   return (
     <Router>
         <Routes>
-<<<<<<< HEAD
           <Route path='/login'          element={<Login/>}       />
           <Route path='/register'       element={<Register/>}    />
           <Route path='/feed'           element={<MainPage/>}    />
@@ -18,17 +14,9 @@ function App() {
           <Route path='/profile'        element={<Profile/>}     />
           <Route path='*'               element={<NotFound404/>} />
           <Route path='account-restore' element={<AccountRestore/>} />
-=======
-          <Route path='/login'        element={<Login/>}       />
-          <Route path='/register'     element={<Register/>}    />
-          <Route path='/feed'         element={<MainPage/>}    />
-          <Route path='/chats'        element={<Chats/>}       />
-          <Route path='/profile'      element={<Profile/>}     />
           <Route path='/profile-edit' element={<ProfileEdit/>} />
           <Route path='/battles'      element={<Battles/>}     />
           <Route path='/f.a.q'        element={<FAQ/>}         />
-          <Route path='*'             element={<NotFound404/>} />
->>>>>>> 2ac0169e6b39cfc6c319024a7e127f03a2b55e63
         </Routes>
     </Router>
   );
--- src/components/pages/pages.js
+++ src/components/pages/pages.js
@@ -4,14 +4,10 @@ import NotFound404 from "./NotFound404/NotFound404";
 import MainPage from "./MainPage/MainPage";
 import Chats from "./Chats/Chats";
 import Profile from "./Profile/Profile";
-<<<<<<< HEAD
 import AccountRestore from "./AccountRestore/AccountRestore";
-
-export {Login, Register, NotFound404, MainPage, Chats, Profile, AccountRestore};
-=======
 import ProfileEdit from "./ProfileEdit/ProfileEdit";
 import Battles from "./Battles/Battles";
 import FAQ from "./FAQ/FAQ";
 
-export {Login, Register, NotFound404, MainPage, Chats, Profile, ProfileEdit, Battles, FAQ};
->>>>>>> 2ac0169e6b39cfc6c319024a7e127f03a2b55e63
+export {Login, Register, NotFound404, MainPage, Chats, Profile, ProfileEdit, Battles, FAQ, AccountRestore};
+
