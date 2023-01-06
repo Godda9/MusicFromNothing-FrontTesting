3fc387044ee7ea4129b481542bfdb6d41442b0d4
Bohdan <bogdantelep@gmail.com>
2023-01-06 11:13:03 +0300

    Adding search algms on mainpage

--- src/components/left-bar/left-bar.js
+++ src/components/left-bar/left-bar.js
@@ -1,100 +1,57 @@
 import LeftBarItem from './left-bar-item/left-bar-item';
 import * as Icon from 'react-bootstrap-icons';
+import { useEffect, useState, useTransition } from 'react';
 
 
 const Leftbar = (props) => {
+    const data = [
+        {link: '/login', heading: "a", placeholder: "Track placeholder0", image: 'https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif'},
+        {link: '/login', heading: "b", placeholder: "Track placeholder1", image: 'https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif'},
+        {link: '/login', heading: "c", placeholder: "Track placeholder2", image: 'https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif'},
+        {link: '/login', heading: "d", placeholder: "Track placeholder3", image: 'https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif'},
+        {link: '/login', heading: "e", placeholder: "Track placeholder4", image: 'https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif'},
+    ];
+
+
+    const [isPending, startTransition] = useTransition();
+    const [seachQuery, setSearchQuery] = useState("");
+    const [visibleData, setVisibleData] = useState(data);
+
+    const filterData = (text) => {
+        if (text.length === 0) { 
+            return data;
+        }
+        return data.filter(item => item.heading.indexOf(text) !== -1);
+    }
+
+    useEffect(() => {
+        startTransition(() => {
+            setVisibleData(filterData(seachQuery));
+        })
+    }, [seachQuery]);
+
+
     return (
         <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white w-100 overflow-auto">
             <div className="m-3 d-flex align-items-center justify-content-between position-relative">
                 <Icon.Search width='1.8em' height='1.8em' style={{marginRight: '0.5em'}}/>
-                <input className="form-control rounded" type="text" placeholder='Tracks' />       
+                <input className="form-control rounded" type="text" placeholder='Tracks' onChange={(e) => setSearchQuery(e.target.value)} />       
             </div>
 
             <div className="list-group list-group-flush border-bottom scrollarea">
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif"
-                />  
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.gifmagazine.net/gifmagazine/images/2791875/original.gif"
-                /> 
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.freepik.com/premium-vector/bright-glossy-saturn-cute-planet-pixel-art-style-space-background_88653-1372.jpg?w=360"
-                />
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif"
-                />  
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.gifmagazine.net/gifmagazine/images/2791875/original.gif"
-                /> 
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.freepik.com/premium-vector/bright-glossy-saturn-cute-planet-pixel-art-style-space-background_88653-1372.jpg?w=360"
-                /> 
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif"
-                />  
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.gifmagazine.net/gifmagazine/images/2791875/original.gif"
-                /> 
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.freepik.com/premium-vector/bright-glossy-saturn-cute-planet-pixel-art-style-space-background_88653-1372.jpg?w=360"
-                /> 
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://i.pinimg.com/originals/fa/e1/bc/fae1bce61f7b0403666fd894298488ee.gif"
-                />  
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.gifmagazine.net/gifmagazine/images/2791875/original.gif"
-                /> 
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.freepik.com/premium-vector/bright-glossy-saturn-cute-planet-pixel-art-style-space-background_88653-1372.jpg?w=360"
-                />  
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track heading" 
-                    placeholder="Track placeholder"
-                    image="https://img.gifmagazine.net/gifmagazine/images/2791875/original.gif"
-                /> 
-                <LeftBarItem 
-                    link="/login" 
-                    heading="Track headingEND" 
-                    placeholder="Track placeholder"
-                    image="https://img.freepik.com/premium-vector/bright-glossy-saturn-cute-planet-pixel-art-style-space-background_88653-1372.jpg?w=360"
-                /> 
+                    {
+                        visibleData.map((item, index) => {
+                            return (
+                                <LeftBarItem 
+                                    link={item.link}
+                                    heading={item.heading}
+                                    placeholder={item.placeholder}
+                                    image={item.image}
+                                    key={index}
+                                />
+                            );
+                        })         
+                    }
             </div>
         </div>
     );
--- src/components/right-bar/right-bar.js
+++ src/components/right-bar/right-bar.js
@@ -1,75 +1,56 @@
 import RightBarItem from './right-bar-item/right-bar-item';
 import * as Icon from 'react-bootstrap-icons';
+import {useState, useTransition, useEffect} from "react";
 
 const Rightbar = (props) => {
+    const data = [
+        {link: '/login', heading: "a", placeholder: "Account description0", image: 'https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g'},
+        {link: '/login', heading: "b", placeholder: "Account description1", image: 'https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g'},
+        {link: '/login', heading: "c", placeholder: "Account description2", image: 'https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g'},
+        {link: '/login', heading: "d", placeholder: "Account description3", image: 'https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g'},
+        {link: '/login', heading: "e", placeholder: "Account description4", image: 'https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g'},
+    ];
+
+
+    const [isPending, startTransition] = useTransition();
+    const [seachQuery, setSearchQuery] = useState("");
+    const [visibleData, setVisibleData] = useState(data);
+
+    const filterData = (text) => {
+        if (text.length === 0) { 
+            return data;
+        }
+        return data.filter(item => item.heading.indexOf(text) !== -1);
+    }
+
+    useEffect(() => {
+        startTransition(() => {
+            setVisibleData(filterData(seachQuery));
+        })
+    }, [seachQuery]);
+
+
     return (
         <div className="d-flex flex-column align-items-stretch flex-shrink-0 bg-white w-100 overflow-auto">
             <div className="m-3 d-flex flex-row-reverse align-items-center justify-content-between position-relative">
-                <input className="form-control rounded" type="text" placeholder='People' />       
+                <input className="form-control rounded" type="text" placeholder='People' onChange={(e) => setSearchQuery(e.target.value)}/>       
                 <Icon.Search width='1.8em' height='1.8em' style={{marginRight: '0.5em'}}/>
             </div>
 
             <div className="list-group list-group-flush border-bottom scrollarea">
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
-                <RightBarItem 
-                    link="/login" 
-                    heading="User Nickname" 
-                    placeholder="Account description"
-                    image="https://secure.gravatar.com/avatar/4c2d5681a4633a173c20e74c3641e637?s=500&d=mm&r=g"
-                />  
+                {
+                    visibleData.map((item, index) => {
+                        return (
+                            <RightBarItem 
+                                link={item.link} 
+                                heading={item.heading} 
+                                placeholder={item.placeholder}
+                                image={item.image}
+                                key={index}
+                            />  
+                        );
+                    })
+                }
                 
             </div>
         </div>
