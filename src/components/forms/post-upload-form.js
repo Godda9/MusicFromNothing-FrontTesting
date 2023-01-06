47a2a9d3a8862f45bc9e39d560ae71797fde15d1
Bohdan <bogdantelep@gmail.com>
2023-01-06 22:17:45 +0300

    minor changes

--- src/components/forms/post-upload-form.js
+++ src/components/forms/post-upload-form.js
@@ -12,8 +12,8 @@ const PostUploadForm = (props)=> {
 
     return(
         <>  
-            <form onSubmit={handleSubmit(onSubmit)} className='column'>
-                <div className='container gap-4'>
+            <div className="bordered row border overflow-hidden shadow d-flex justify-content-center align-items-center w-50 p-5 m-5">
+                <form onSubmit={handleSubmit(onSubmit)} className='column'>
                     <div className="mb-3">
                         <label htmlFor="Title" className="form-label"><b>Title</b></label>
                         <input 
@@ -43,59 +43,59 @@ const PostUploadForm = (props)=> {
                         />
                         {errors.ShortDesc && <span className="form-text text-danger">Short description must be from 4 to 20 characters</span>}
                     </div>
-                </div>
-                
-                
-                <div className="pt-3 w-50">
-                    <label htmlFor="MusicType" className="form-label"><b>Music Type:</b></label>
-                    <div class="dropdown">
-                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                            Select
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-                            <a className="dropdown-item" selected href="#">Action</a>
-                            <a className="dropdown-item" href="#">Another action</a>
-                            <a className="dropdown-item" href="#">Something else here</a>
+                    
+    
+                    <div className="pt-3 w-50">
+                        <label htmlFor="MusicType" className="form-label"><b>Music Type:</b></label>
+                        <div className="dropdown">
+                            <button className="btn btn-outline-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                Select
+                            </button>
+                            <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                <a className="dropdown-item" selected href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another action</a>
+                                <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
                         </div>
                     </div>
-                
-                </div>
-                
-                <div className='container'>
-                    <div className='w-50'>
-                        <label htmlFor="UploadImage" className="form-label"><b>Image:</b></label>
-                        <input className="form-control" type="file" id="formFile" placeholder="Upload"/>
-                    </div>
-
-                    <div className='w-50'>
-                        <label htmlFor="ChooseAudio" className="form-label"><b>Audio</b></label>
-                        <input className="form-control" type="file" id="formFile" placeholder="Choose from creations"/>
-                    </div>
-
-                    <div className='w-50'>
-                        <input placeholder="Upload" className="form-control" type="file" id="formFile" />
-                    </div>
-                </div>
-                
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <div class="input-group">
-                            <input type="checkbox" aria-label="Checkbox for following text input"/>
+                    
+                    <div className='w-100'>
+                        <div className="pt-3">
+                            <label htmlFor="UploadImage" className="form-label"><b>Image</b></label>
+                            <input className="form-control" type="file" id="formFile" placeholder="Upload"/>
                         </div>
                     </div>
-                    <label htmlFor="AllowComments" className="form-label">Allow comments</label>
-                </div>
-                <div class="input-group mb-3">
-                    <div class="input-group-prepend">
-                        <div class="input-group">
-                            <input type="checkbox" aria-label="Checkbox for following text input"/>
+                    <div className='w-100 d-flex justify-content-between align-items-center'>
+                        <div className="pt-3">
+                            <label htmlFor="ChooseAudio" className="form-label"><b>Audio</b></label>
+                            <input className="form-control" type="file" id="formFile" placeholder="Choose from creations"/>
+                        </div>
+                        <div className="pt-3">
+                            <label htmlFor="ChooseAudio" className="form-label"><b>Choose own audio</b></label>
+                            <input placeholder="Upload" className="form-control" type="file" id="formFile" />
                         </div>
                     </div>
-                    <label htmlFor="AllowDownaloads" className="form-label">Allow downloads</label>
-                </div>
+                    
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <div className="input-group">
+                                <input type="checkbox" aria-label="Checkbox for following text input"/>
+                            </div>
+                        </div>
+                        <label htmlFor="AllowComments" className="form-label">Allow comments</label>
+                    </div>
+                    <div className="input-group mb-3">
+                        <div className="input-group-prepend">
+                            <div className="input-group">
+                                <input type="checkbox" aria-label="Checkbox for following text input"/>
+                            </div>
+                        </div>
+                        <label htmlFor="AllowDownaloads" className="form-label">Allow downloads</label>
+                    </div>
 
-                <button type="submit" className="btn btn-primary">Post</button>
-            </form>
+                    <button type="submit" className="btn btn-primary">Post</button>
+                </form>
+            </div>
         </>
     );
 }
--- src/components/pages/PostUpload/PostUpload.js
+++ src/components/pages/PostUpload/PostUpload.js
@@ -1,24 +1,36 @@
 import Topbar from "../../top-bar/top-bar";
 import PostUploadForm from "../../forms/post-upload-form";
-import PostItem from "../../post-item/post-item";
+import PostItemDynamicPreview from "../../post-item-dynamic-preview/post-item-dynamic-preview";
 
 const PostUpload = (props) => {
     return (
         <>
-            <div className="container-fluid position-sticky">
-                <div className="row">
+            <div className="container-fluid">
+                <div className="row position-sticky top-0">
                     <Topbar text="Post Upload" username="UserName" where="post-upload"/>
                 </div>
-                <div className="d-flex justify-content-center align-items-center">
-                    <div>
-                        <div className="row d-flex justify-content-center align-items-center m-5">
-                            FORM
-                            <div>
-                                <PostUploadForm/>
-                            </div>
+                <div className="col overflow-y-auto" style={{maxHeight: '93vh'}}>
+                    <div className="row d-flex justify-content-center align-items-center">
+                        <h1 style={{textAlign: 'center'}}><span style={{'color': '#1BA39C'}}>P</span>ost upload</h1>
+                        <span className="text-center fs-1">Set up your post using super-form below:</span>
+                    </div>
+                    <div className="row">
+                        <div className="col p-3 d-flex justify-content-center align-items-center h-50 mx-5">
+                            <PostUploadForm/>
+                            <PostItemDynamicPreview 
+                                user="UserName"
+                                datetime="01.01.23"
+                                title="Track title"
+                                description="Track description..."
+                                isLiked={true}
+                                img="https://iso.500px.com/wp-content/uploads/2017/10/500px_Golden_Hour_Quest_winner-1500x1000.jpg"
+                                likes={120}
+                                comments={53}
+                                status={null}
+                            />
                         </div>
                     </div>
-                </div> 
+                </div>
             </div>
         </>
     );
