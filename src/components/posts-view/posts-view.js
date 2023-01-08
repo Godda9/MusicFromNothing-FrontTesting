d45abc2d28d488bc3dd867078a15c5057a8e38c0
Bohdan <bogdantelep@gmail.com>
2023-01-08 14:33:43 +0300

    adding some stuff to comments block

--- src/components/posts-view/posts-view.js
+++ src/components/posts-view/posts-view.js
@@ -8,10 +8,8 @@ const PostsView = (props) => {
 
     return (
         <>
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 m-2">
-                { what === "posts" ? <PostsBlock onDataChanged={onDataChanged}/> : null }
-                { what === "comments" ? <CommentsBlock commentsId={commentsId} switchDataType={switchDataType}/> : null }
-            </div>
+            { what === "posts" ? <PostsBlock onDataChanged={onDataChanged}/> : null }
+            { what === "comments" ? <CommentsBlock commentsId={commentsId} switchDataType={switchDataType}/> : null }
         </>
     );
 }
@@ -21,29 +19,31 @@ const PostsBlock = (props) => {
     const {onDataChanged} = props;
     return(
         <>
-            <PostItem 
-                user="UserName" 
-                datetime="01.01.23" 
-                title="Track title" 
-                description="Track description..."
-                isLiked={true}
-                img="https://iso.500px.com/wp-content/uploads/2017/10/500px_Golden_Hour_Quest_winner-1500x1000.jpg"
-                likes={120}
-                comments={53}
-                status={null}
-                onCommentsClick={onDataChanged}
-            />
-            <PostItemSkeleton 
-                user="UserName" 
-                datetime="01.01.23" 
-                title="Track title" 
-                description="Track description..."
-                isLiked={false}
-                img="https://iso.500px.com/wp-content/uploads/2017/10/500px_Golden_Hour_Quest_winner-1500x1000.jpg"
-                likes={120}
-                comments={53}
-                status={null}
-            />
+            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 m-2">
+                <PostItem 
+                    user="UserName" 
+                    datetime="01.01.23" 
+                    title="Track title" 
+                    description="Track description..."
+                    isLiked={true}
+                    img="https://iso.500px.com/wp-content/uploads/2017/10/500px_Golden_Hour_Quest_winner-1500x1000.jpg"
+                    likes={120}
+                    comments={53}
+                    status={null}
+                    onCommentsClick={onDataChanged}
+                />
+                <PostItemSkeleton 
+                    user="UserName" 
+                    datetime="01.01.23" 
+                    title="Track title" 
+                    description="Track description..."
+                    isLiked={false}
+                    img="https://iso.500px.com/wp-content/uploads/2017/10/500px_Golden_Hour_Quest_winner-1500x1000.jpg"
+                    likes={120}
+                    comments={53}
+                    status={null}
+                />
+            </div>
         </>
     );
 }
@@ -52,10 +52,28 @@ const CommentsBlock = (props) => {
     const {commentsId, switchDataType} = props
     return (
         <>
-            <div>
-                COMMENTS POST ID {commentsId}
+            <div className='row g-0 position-sticky top-0 shadow' style={{zIndex: 1}}>
+                <button className='btn btn-primary btn-sm' onClick={switchDataType}>Close Comments</button>
             </div>
-            <button onClick={switchDataType}>Back</button>
+            <div className="row g-0 overflow-hidden flex-md-row my-5 position-relative d-flex justify-content-center align-items-center">
+                <PostItem 
+                    user="UserName" 
+                    datetime="01.01.23" 
+                    title="Track title" 
+                    description="Track description..."
+                    isLiked={true}
+                    img="https://iso.500px.com/wp-content/uploads/2017/10/500px_Golden_Hour_Quest_winner-1500x1000.jpg"
+                    likes={120}
+                    comments={53}
+                    status={null}
+                />
+            </div>
+            
+            <div className="row row-cols-mb-5 g-5 m-3">
+                <div>COMMENTS POST {commentsId} WITH ALL MEDIA DATA</div>
+                <div>COMMENTS POST {commentsId} WITH ALL MEDIA DATA</div>
+                <div>COMMENTS POST {commentsId} WITH ALL MEDIA DATA</div>
+            </div>
         </>
     );
 }
