62fb5c272a07724826cd2e3d13930628244b48d1
Bohdan <bogdantelep@gmail.com>
2023-01-17 17:26:57 +0300

    rm search bar bottom

--- src/components/pages/Container/Container.js
+++ src/components/pages/Container/Container.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import Topbar from "../../top-bar/top-bar";
 import Leftbar from "../../left-bar/left-bar";
 import Rightbar from "../../right-bar/right-bar";
-import SearchBarBottom from "../../seacrh-bar-bottom/search-bar-bottom";
+
 
 
 const Container = (props) => {
@@ -19,8 +19,6 @@ const Container = (props) => {
                     <Outlet/>
                 </div>     
             </div>
-            
-            <SearchBarBottom/>
         </div>
     );
 }
