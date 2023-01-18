2c6ef11b9b066387c6e7bb707b6ea779fa773b39
Bohdan <bogdantelep@gmail.com>
2023-01-18 23:25:28 +0300

    18.01.23.23.25

--- src/components/battle-container/batlle-container.js
+++ src/components/battle-container/batlle-container.js
@@ -4,17 +4,19 @@ const BattleContainer = (props) => {
     const {post1, post2, endsIn} = props;
     return (
         <>
-            <div className="d-flex justify-content-around battle-info-head">
-                <span className="fs-3">Post1_Name</span>
-                <span className="fs-3"><b>VS</b></span>
-                <span className="fs-3">Post2_Name</span>
-            </div>
-            <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 m-2 d-flex justify-content-around">
-                <div className="col-md-5">
-                    {post1}
+            <div className='bordered border p-2 m-2'>
+                <div className="d-flex justify-content-around battle-info-head">
+                    <span className="fs-3">Post1_Name</span>
+                    <span className="fs-3"><b>VS</b></span>
+                    <span className="fs-3">Post2_Name</span>
                 </div>
-                <div className="col-md-5">
-                    {post2}
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 m-2 d-flex justify-content-around">
+                    <div className="col-md-5">
+                        {post1}
+                    </div>
+                    <div className="col-md-5">
+                        {post2}
+                    </div>
                 </div>
             </div>
         </>
