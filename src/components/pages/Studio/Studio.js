4fd04aae43ef44a3567bdc5e909e50bc3b9bec86
Bohdan <bogdantelep@gmail.com>
2023-01-24 23:27:54 +0300

    24.01.23.22.39

--- src/components/pages/Studio/Studio.js
+++ src/components/pages/Studio/Studio.js
@@ -1,19 +1,18 @@
 import Drumpad from "../../studio/drumpad/drumpad";
+import Piano from "../../studio/piano/piano";
 
 import Topbar from "../../top-bar/top-bar";
 
 const Studio = (props) => {
-    if (window.innerWidth > 1900 || window.innerHeight > 1000) {
+    
         return (
             <>
-                <div className="container-fluid position-fixed">
-                    <div className="row">
-                        <Topbar text="Studio" username="UserName" where="studio"/>
-                    </div>
+                <div className="container-fluid">
+                    
                     <div className="row">
     
                         <div className="col col-lg-2">
-                            <div className="row" style={{minHeight: '95vh'}}>
+                            <div className="row">
                                 abc
                             </div>
                         </div>
@@ -22,12 +21,12 @@ const Studio = (props) => {
                             <div className="row border border-dark" style={{minHeight: '50vh'}}>
                                 MAIN VIEW
                             </div>
-                            <div className="row border border-dark" style={{minHeight: '30vh'}}>
+                            <div className="row border border-dark p-0">
                                 <div className="col">
                                     EQ
                                 </div>
                                 <div className="col">
-                                    
+                                    <Piano/>
                                 </div>
                                 <div className="col">
                                     <Drumpad/>
@@ -40,7 +39,7 @@ const Studio = (props) => {
                         </div>
     
                         <div className="col col-lg-2">
-                            <div className="row" style={{minHeight: '95vh'}}>
+                            <div className="row">
                                 GUITAR
                             </div>
                         </div>
@@ -49,14 +48,8 @@ const Studio = (props) => {
                 </div>
             </>
         );
-    } else {
-        return (
-            <>
-                NOT SUPPORTED WITH CURRENT WINDOW WIDTH / HEIGHT
-            </>
-        );
-    }
+    } 
 
-}
+
 
 export default Studio;
---
+++ src/components/studio/piano/piano.js
@@ -0,0 +1,29 @@
+import './piano.scss';
+
+const Piano = (props) => {
+    return (
+        <div className="wrapper">
+            <ul className="piano-keys">
+                <li className="key white" data-key="a"><span>a</span></li>
+                <li className="key black" data-key="w"><span>w</span></li>
+                <li className="key white" data-key="s"><span>s</span></li>
+                <li className="key black" data-key="e"><span>e</span></li>
+                <li className="key white" data-key="d"><span>d</span></li>
+                <li className="key white" data-key="f"><span>f</span></li>
+                <li className="key black" data-key="t"><span>t</span></li>
+                <li className="key white" data-key="g"><span>g</span></li>
+                <li className="key black" data-key="y"><span>y</span></li>
+                <li className="key white" data-key="h"><span>h</span></li>
+                <li className="key black" data-key="u"><span>u</span></li>
+                <li className="key white" data-key="j"><span>j</span></li>
+                <li className="key white" data-key="k"><span>k</span></li>
+                <li className="key black" data-key="o"><span>o</span></li>
+                <li className="key white" data-key="l"><span>l</span></li>
+                <li className="key black" data-key="p"><span>p</span></li>
+                <li className="key white" data-key=";"><span>;</span></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Piano;
